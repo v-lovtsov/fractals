8336312079c7cc227ab1eaeb6f1cf2abe6ebe691
v-lovtsov <v.lovtsov@g.nsu.ru>
2024-10-22 22:39:02 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,21 +6,21 @@ t.speed(5)
 def draw(angle, size, step):
   if step == 1:
     t.right(angle)
-
+   
   t.forward(size)
   t.right(90)
   
-  if size == 0:
+  if size <= 0:
     return
   
   if step == 4:
-    t.forward(size / 20)
-    return draw(angle + 2, size - 10, 1)
+    t.forward(size / 15)
+    return draw(angle + 2, size - 15, 1)
   
   return draw(angle, size, step + 1)
   	
 
 def main():
-	draw(0, 100, 1)
+	draw(0, 200, 1)
   
 main()
